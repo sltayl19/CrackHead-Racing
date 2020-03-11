8adee17a4cfbbba4c90ce3c9e3bfa8a0bc37d4e2
Steven <60404094+sltayl19@users.noreply.github.com>
2020-03-11 14:16:17 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,17 +16,15 @@ img1= pyglet.image.load('Legacy/PNG/roadTile11.png')
 spr1 = pyglet.sprite.Sprite(img1, x = 0, y = 0)
 
 
-img= pyglet.image.load('isometric_vehicles/green_vehicles.png')
-img = img.get_region(x=96, y=0, width=32, height=32)
+img= pyglet.image.load('isometric_vehicles/white_vehicles.png')
+img = img.get_region(x=162, y=192, width=32, height=32)
 spr = pyglet.sprite.Sprite(img, x = 200, y = 200)
-spr.scale = 3
+spr.scale = 2
 
 
 # Start the event loop
 def update(dt):  
      win.clear()
-
-
      if keys[pyglet.window.key.LEFT]:
       spr.x -= 2
      if keys[pyglet.window.key.RIGHT]:
@@ -40,8 +38,8 @@ def update(dt):
 def on_draw():
     win.clear()
     util.pixelScale()
-    spr.draw()
     spr1.draw()
+    spr.draw()
     
 
 
