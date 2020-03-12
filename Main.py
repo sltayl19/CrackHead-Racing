7674b236eef36ad2fb288bb3aadf05d42799cb81
Steven <60404094+sltayl19@users.noreply.github.com>
2020-03-12 14:34:24 -0400

    Update Main.py
    spr4 moves

--- Main.py
+++ Main.py
@@ -14,16 +14,15 @@ print(Style.NORMAL)
 
 img1= pyglet.image.load('Legacy/PNG/roadTile11.png')
 spr1 = pyglet.sprite.Sprite(img1, x = 0, y = 0)
-spr1 = []
-for i in range(14):
-  spr1.append(pyglet.sprite.Sprite(img, x = 64, j = 64))
-
 
 
 
 img2= pyglet.image.load('Legacy/PNG/roadTile13.png')
 spr2 = pyglet.sprite.Sprite(img2, x = 0, y = 64)
 
+img4= pyglet.image.load('Legacy/PNG/recycle_items.png')
+img4 = img4.get_region(x=0, y=0, width=19, height=48)
+spr4 = pyglet.sprite.Sprite(img4, x = 200, y = 200)
 
 
 
@@ -31,6 +30,15 @@ img= pyglet.image.load('isometric_vehicles/white_vehicles.png')
 img = img.get_region(x=162, y=192, width=32, height=32)
 spr = pyglet.sprite.Sprite(img, x = 200, y = 200)
 spr.scale = 2
+
+
+
+img3= pyglet.image.load('isometric_vehicles/green_vehicles.png')
+img3 = img3.get_region(x=162, y=192, width=32, height=32)
+spr3 = pyglet.sprite.Sprite(img3, x = 200, y = 200)
+spr3.scale = 2
+
+
 
 
 # Start the event loop
@@ -45,12 +53,29 @@ def update(dt):
      if keys[pyglet.window.key.DOWN]:
        spr.y -=2
 
+
+     if keys[pyglet.window.key.A]:
+      spr3.x -= 2
+     if keys[pyglet.window.key.D]:
+      spr3.x += 2
+     if keys[pyglet.window.key.W]:
+      spr3.y += 2
+     if keys[pyglet.window.key.S]:
+       spr3.y -=2
+
+
+
+
+
+
+
 @win.event
 def on_draw():
     win.clear()
     util.pixelScale()
-
     spr1.draw()
+    spr3.draw()
+    spr4.draw()
     spr2.draw()
     spr.draw()
     
